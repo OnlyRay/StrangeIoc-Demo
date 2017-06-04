fde1ea42359e8677d952dc7c8d85fb7cd3846875
OnlyRay <534755973@qq.com>
2017-06-04 17:00:28 +0800

    StrangeIoc

---
+++ Assets/Demo1/Scripts/Command/StartCommand.cs
@@ -0,0 +1,14 @@
+﻿using strange.extensions.command.impl;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCommand : Command {
+    //开始命令
+    //当命令被执行的时候，默认调用execute方法
+    public override void Execute()
+    {
+        Debug.Log("start command execute");
+    }
+
+}
